e4982921d9cbb2617ca9f86a31efdbed9b15678e
Tereska90 <teresa.esteban1990@gmail.com>
2023-07-09 14:51:42 +0200

    update con librerias

---
+++ Codigo/CargarPGANFINAL.py
@@ -0,0 +1,360 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 18:42:33 2023
+
+@author: bowman
+"""
+#PRIMERO: SE CARGAN LOS MODELOS PREVIAMENTE GUARDADDOS (GENENADOR Y DISCRIMINADOR). HAY QUE ESPECIFICAR LAS RUTAS 
+#SEGUNDO: SE GENERAN UN NÚMERO n DE IMÁGENES. n LO DECIDIMOS NOSOTROS MODIFICANDO EL VALOR DEL PARÁMETRO BATCH SIZES (32 POR DEFECTO) 
+#LIBRERIAS
+#os: Este es un módulo en Python que proporciona funciones para interactuar con el sistema operativo.
+#torch: Es una biblioteca de Python abierta para Machine Learning.
+#torch.nn: Es un submódulo de PyTorch (torch) que contiene clases para la creación de redes neuronales. 
+#torch.optim: Este es otro submódulo de PyTorch. Proporciona implementaciones de varios algoritmos de optimización que se pueden utilizar para entrenar redes neuronales.
+#torch.nn.functional: Contiene muchas funciones que no son métodos de ninguna clase y se pueden usar de manera independiente. Por ejemplo, contiene funciones de activación (como ReLU, tanh), funciones de pérdida, etc.
+#torchvision.utils: Es parte de torchvision, que es una biblioteca de PyTorch para trabajar con imágenes.
+#tqdm: Es una biblioteca de Python para mostrar barras de progreso en un bucle for.
+#matplotlib.pylab: Es una biblioteca de trazado en Python que proporciona una interfaz similar a MATLAB.
+#pandas: Es una biblioteca de manipulación y análisis de datos en Python. 
+
+
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+from torchvision.utils import save_image
+from tqdm import tqdm
+import matplotlib.pylab as plt
+import pandas as pd
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+LR = 1e-3
+BATCH_SIZES = 12   #CAMBIAR EL MÚMERO DE IMÁGENES GENERADAS
+CHANNELS_IMG = 3
+Z_DIm = 512
+W_DIM = 512
+IN_CHANNELS = 512
+LAMBDA_GP = 10
+
+PROGRESSIVE_EPOCHS = [30] 
+
+
+#PRIMERO: SE CARGAN LOS MODELOS PREVIAMENTE GUARDADDOS (GENENADOR Y DISCRIMINADOR). HAY QUE ESPECIFICAR LAS RUTAS
+path_models = 'path_to_models'
+
+path_gen = os.join(path_models, "generator_abstracto512.pt")
+path_critic= os.join(path_models, "critic_abstracto512.pt")
+
+factors = [1,1,1,1/2,1/4,1/8,1/16,1/32,1/64,1/128]
+class WSLinear(nn.Module):
+    def __init__(
+        self, in_features, out_features
+    ):
+        super(WSLinear,self).__init__()
+        self.linear = nn.Linear(in_features, out_features)
+        self.scale  = (2/in_features) ** 0.5
+        self.bias   = self.linear.bias
+        self.linear.bias = None
+
+        nn.init.normal_(self.linear.weight)
+        nn.init.zeros_(self.bias)
+
+    def forward(self,x):
+        return self.linear(x * self.scale) + self.bias
+class PixenNorm(nn.Module):
+    def __init__(self):
+        super(PixenNorm, self).__init__()
+        self.epsilon = 1e-8
+    def forward(self,x ):
+        return x / torch.sqrt(torch.mean(x**2, dim=1, keepdim=True)+  self.epsilon)
+class MappingNetwork(nn.Module):
+    def __init__(self, z_dim, w_dim):
+        super().__init__()
+        self.mapping = nn.Sequential(
+            PixenNorm(),
+            WSLinear(z_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+            nn.ReLU(),
+            WSLinear(w_dim, w_dim),
+        )
+    
+    def forward(self,x):
+        return self.mapping(x)
+class AdaIN(nn.Module):
+    def __init__(self, channels, w_dim):
+        super().__init__()
+        self.instance_norm = nn.InstanceNorm2d(channels)
+        self.style_scale   = WSLinear(w_dim, channels)
+        self.style_bias    = WSLinear(w_dim, channels)
+
+    def forward(self,x,w):
+        x = self.instance_norm(x)
+        style_scale = self.style_scale(w).unsqueeze(2).unsqueeze(3)
+        style_bias  = self.style_bias(w).unsqueeze(2).unsqueeze(3)
+        return style_scale * x + style_bias
+class injectNoise(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.weight = nn.Parameter(torch.zeros(1,channels,1,1))
+
+    def forward(self, x):
+        noise = torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]), device = x.device)
+        return x + self.weight + noise
+class GenBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, w_dim):
+        super(GenBlock, self).__init__()
+        self.conv1 = WSConv2d(in_channel, out_channel)
+        self.conv2 = WSConv2d(out_channel, out_channel)
+        self.leaky = nn.LeakyReLU(0.2, inplace=True)
+        self.inject_noise1 = injectNoise(out_channel)
+        self.inject_noise2 = injectNoise(out_channel)
+        self.adain1 = AdaIN(out_channel, w_dim)
+        self.adain2 = AdaIN(out_channel, w_dim)
+    def forward(self, x,w):
+        x = self.adain1(self.leaky(self.inject_noise1(self.conv1(x))), w)
+        x = self.adain2(self.leaky(self.inject_noise2(self.conv2(x))), w)
+        return x
+class Generator(nn.Module):
+    def __init__(self, z_dim, w_dim, in_channels, img_channels=3):
+        super().__init__()
+        self.starting_cte = nn.Parameter(torch.ones(1, in_channels, 4,4))
+        self.map = MappingNetwork(z_dim, w_dim)
+        self.initial_adain1 = AdaIN(in_channels, w_dim)
+        self.initial_adain2 = AdaIN(in_channels, w_dim)
+        self.initial_noise1 = injectNoise(in_channels)
+        self.initial_noise2 = injectNoise(in_channels)
+        self.initial_conv   = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1)
+        self.leaky          = nn.LeakyReLU(0.2, inplace=True)
+
+        self.initial_rgb    = WSConv2d(
+            in_channels, img_channels, kernel_size = 1, stride=1, padding=0
+        )
+        self.prog_blocks, self.rgb_layers = (
+            nn.ModuleList([]),
+            nn.ModuleList([self.initial_rgb])
+        )
+
+        for i in range(len(factors)-1):
+            conv_in_c  = int(in_channels * factors[i])
+            conv_out_c = int(in_channels * factors[i+1])
+            self.prog_blocks.append(GenBlock(conv_in_c, conv_out_c, w_dim))
+            self.rgb_layers.append(WSConv2d(conv_out_c, img_channels, kernel_size = 1, stride=1, padding=0))
+        
+    def fade_in(self, alpha, upscaled, generated):
+        return torch.tanh(alpha * generated + (1-alpha ) * upscaled)
+
+    def forward(self, noise, alpha, steps):
+        w = self.map(noise)
+        x = self.initial_adain1(self.initial_noise1(self.starting_cte),w)
+        x = self.initial_conv(x)
+        out = self.initial_adain2(self.leaky(self.initial_noise2(x)), w)
+
+        if steps == 0:
+            return self.initial_rgb(x)
+        
+        for step in range(steps):
+            upscaled = F.interpolate(out, scale_factor=2, mode = 'bilinear')
+            out      = self.prog_blocks[step](upscaled,w)
+
+        final_upscaled = self.rgb_layers[steps-1](upscaled)
+        final_out      = self.rgb_layers[steps](out)
+
+        return self.fade_in(alpha, final_upscaled, final_out)
+class WSConv2d(nn.Module):
+    def __init__(
+        self, in_channels, out_channels, kernel_size=3, stride=1, padding=1
+    ):
+        super(WSConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.scale = (2 / (in_channels * (kernel_size ** 2))) ** 0.5
+        self.bias = self.conv.bias
+        self.conv.bias = None      
+        nn.init.normal_(self.conv.weight)
+        nn.init.zeros_(self.bias)
+    def forward(self, x):
+        return self.conv(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.conv1 = WSConv2d(in_channels, out_channels)
+        self.conv2 = WSConv2d(out_channels, out_channels)
+        self.leaky = nn.LeakyReLU(0.2)
+    def forward(self, x):
+        x = self.leaky(self.conv1(x))
+        x = self.leaky(self.conv2(x))
+        return x
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, img_channels=3):
+        super(Discriminator, self).__init__()
+        self.prog_blocks, self.rgb_layers = nn.ModuleList([]), nn.ModuleList([])
+        self.leaky = nn.LeakyReLU(0.2)
+        for i in range(len(factors) - 1, 0, -1):
+            conv_in = int(in_channels * factors[i])
+            conv_out = int(in_channels * factors[i - 1])
+            self.prog_blocks.append(ConvBlock(conv_in, conv_out))
+            self.rgb_layers.append(
+                WSConv2d(img_channels, conv_in, kernel_size=1, stride=1, padding=0)
+            )       
+        self.initial_rgb = WSConv2d(
+            img_channels, in_channels, kernel_size=1, stride=1, padding=0
+        )
+        self.rgb_layers.append(self.initial_rgb)
+        self.avg_pool = nn.AvgPool2d(
+            kernel_size=2, stride=2
+        ) 
+      
+        self.final_block = nn.Sequential(
+            
+            WSConv2d(in_channels + 1, in_channels, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=4, padding=0, stride=1),
+            nn.LeakyReLU(0.2),
+            WSConv2d(
+                in_channels, 1, kernel_size=1, padding=0, stride=1
+            ),  
+        )
+    def fade_in(self, alpha, downscaled, out):
+              
+        return alpha * out + (1 - alpha) * downscaled
+    def minibatch_std(self, x):
+        batch_statistics = (
+            torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
+        )
+        
+        return torch.cat([x, batch_statistics], dim=1)
+    def forward(self, x, alpha, steps):
+        cur_step = len(self.prog_blocks) - steps
+        out = self.leaky(self.rgb_layers[cur_step](x))
+        if steps == 0:  
+            out = self.minibatch_std(out)
+            return self.final_block(out).view(out.shape[0], -1)
+        downscaled = self.leaky(self.rgb_layers[cur_step + 1](self.avg_pool(x)))
+        out = self.avg_pool(self.prog_blocks[cur_step](out))
+        out = self.fade_in(alpha, downscaled, out)
+        for step in range(cur_step + 1, len(self.prog_blocks)):
+            out = self.prog_blocks[step](out)
+            out = self.avg_pool(out)
+        out = self.minibatch_std(out)
+        return self.final_block(out).view(out.shape[0], -1)
+def generate_examples(gen, steps, n=100):
+    gen.eval()
+    alpha = 1.0
+    for i in range(n):
+        with torch.no_grad():
+            noise = torch.randn(1, Z_DIm).to(DEVICE)
+            img = gen(noise, alpha, steps)
+            if not os.path.exists(f'saved_examples/step{steps}'):
+                os.makedirs(f'saved_examples/step{steps}')
+            save_image(img*0.5+0.5, f"saved_examples/step{steps}/img_{i}.png")
+    gen.train()
+def gradient_penalty(critic, real, fake, alpha, train_step, device="cpu"):
+    BATCH_SIZE, C, H, W = real.shape
+    beta = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * beta + fake.detach() * (1 - beta)
+    interpolated_images.requires_grad_(True)
+    mixed_scores = critic(interpolated_images, alpha, train_step)
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+def train_fn(
+    critic,
+    gen,
+    loader,
+    dataset,
+    step,
+    alpha,
+    opt_critic,
+    opt_gen
+):
+    loop = tqdm(loader, leave=True)
+    for batch_idx, (real, _) in enumerate(loop):
+        real = real.to(DEVICE)
+        cur_batch_size = real.shape[0]
+        noise = torch.randn(cur_batch_size, Z_DIm).to(DEVICE)
+        fake  = gen(noise, alpha, step)
+        critic_real = critic(real, alpha, step)
+        critic_fake = critic(fake.detach(), alpha, step)
+        gp = gradient_penalty(critic, real, fake, alpha, step, DEVICE)
+        loss_critic = (
+            -(torch.mean(critic_real) - torch.mean(critic_fake))
+            + LAMBDA_GP * gp
+            + (0.001) * torch.mean(critic_real ** 2)
+        )
+
+        critic.zero_grad()
+        loss_critic.backward()
+        opt_critic.step()
+        gen_fake = critic(fake, alpha, step)
+        loss_gen = -torch.mean(gen_fake)
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+
+        alpha += cur_batch_size / (
+            PROGRESSIVE_EPOCHS[step] * 0.5 * len(dataset)
+        )
+        alpha = min(alpha,1)
+        loop.set_postfix(
+            gp = gp.item(),
+            loss_critic = loss_critic.item()
+        )
+   
+    return alpha
+
+
+checkpoint = torch.load(path_gen, map_location=torch.device('cpu'))
+
+#SEGUNDO: SE GENERAN UN NÚMERO n DE IMÁGENES. 
+gen = Generator(Z_DIm, W_DIM, IN_CHANNELS, CHANNELS_IMG).to(DEVICE)
+critic = Discriminator(IN_CHANNELS, CHANNELS_IMG).to(DEVICE)
+
+opt_gen = optim.Adam([{'params': [param for name, param in gen.named_parameters() if 'map' not in name]},
+                     {'params': gen.map.parameters(), 'lr': 1e-5}], lr=LR, betas =(0.0, 0.99))
+opt_critic = optim.Adam(
+    critic.parameters(), lr= LR, betas =(0.0, 0.99)
+)
+
+checkpoint = torch.load(path_gen, map_location=torch.device('cpu'))
+gen.load_state_dict(checkpoint['model_state_dict'])
+gen.eval()
+z = torch.randn((BATCH_SIZES, Z_DIm)).to(DEVICE)
+with torch.no_grad(): 
+    alpha = 1.0
+    steps = 7
+    images = gen(z, alpha, steps)
+images = images.to('cpu')
+images = (images + 1) / 2
+images = images.permute(0, 2, 3, 1)
+images = images.numpy()
+# GRABAR LAS IMÁGENES GENERADAS
+output_dir = "C:/Users/vtdac/OneDrive/Desktop/Generadas"
+os.makedirs(output_dir, exist_ok=True)
+for i, img in enumerate(images):
+    plt.imsave(os.path.join(output_dir, f'image_{i}.png'), img)
+print("Imágenes guardadas.")
+
+
+
+
+
